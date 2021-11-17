7f6fb8512f1ce30758aa0fad89ce388bbf678e38
AndrePS <aps.teresopolis@gmail.com>
2021-11-16 21:59:49 -0300

    Aula1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Cliente from './Cliente';
 
-function App() {
+const App = () => {
+  const somaValores = () => {
+    return 2 + 2;
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      teste de compontene
+      {'' + somaValores()}
+      <Cliente />
     </div>
   );
 }
---
+++ src/Cliente.js
@@ -0,0 +1,10 @@
+const Cliente = () => {
+    return (
+        <div style={{ backgroundColor: "aqua" }}>
+         <h1>Dados do cliente</h1>
+         <p>Andre Ponte</p>
+         </div>
+    )
+}
+
+export default Cliente;
