a674b7ad027997cd7a6c7f154e5cc622fb779890
AndrePS <aps.teresopolis@gmail.com>
2021-11-17 17:30:06 -0300

    aula1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import Cliente from './Cliente';
+import ClienteList from './ClienteList';
 
 const App = () => {
-  const somaValores = () => {
-    return 2 + 2;
-  }
-  
   return (
     <div>
-      teste de compontene
-      {'' + somaValores()}
-      <Cliente />
+      <ClienteList />
     </div>
   );
 }
---
+++ src/ClienteList.js
@@ -0,0 +1,13 @@
+import Cliente from './Cliente';
+
+const ClienteList = () => {
+    return (
+        <>
+            <Cliente nome="André" />
+            <Cliente nome="Joãozinho" />
+            <Cliente nome="Mariazinha" />
+        </>
+    )
+}
+
+export default ClienteList;
